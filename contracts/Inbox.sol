5b4a63bf4990aee1325ab159ba217b8b26e7f7ae
Roodelia <weihan@outlook.com>
2021-08-28 09:37:34 +0800

    test initialMessage

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.4.17;
 contract Inbox {
     string public message;  // define storage variable - string type; stored on-chain vs local variable
     
-    function inbox(string initialMessage) public { // constructor function don't need to have same name as contract
+    function Inbox (string initialMessage) public { // constructor function don't need to have same name as contract
         message = initialMessage;
     }
-    
+
     function setMessage(string newMessage) public {
         message = newMessage;
     }
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,3 +5,32 @@ const Web3 = require('web3'); // constructor function using instances of web3 li
 // set up instance; replace provider to link up with Rinkeby or Mainnet
 const web3 = new Web3(ganache.provider());
 
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ 
+            data: bytecode, 
+            arguments: ['Hi there!'] // initial message
+        }) 
+        .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('inbox', () => {
+    it('deploys a contract', ()=> {
+        assert.ok(inbox.options.address); // check if address exist to make sure contract is deployed
+    });
+    
+    it('has default message', async () => { // calling a function is still async
+        const message = await inbox.methods.message().call(); // first () is to pass in the arguments for the function; second () is to customize how the function gets called
+        console.log(message)
+        assert.equal(message, 'Hi there!'); 
+    });
+});
